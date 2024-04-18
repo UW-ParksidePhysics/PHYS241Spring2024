cef09718273229daf1c76bfbcc90cae6520a06e2
Kaitlyn Tombari <158516683+kaitlyntombari@users.noreply.github.com>
2024-04-18 11:39:15 -0500

    Added comments

--- reviews/04.py
+++ reviews/04.py
@@ -126,7 +126,7 @@ else:
 # 2. The output of the code is fairly understandable. The title of the table produced is currently "Star of Output", which I think might be a typo. I would just make the table's name more detailed. 
 #
 #
-# 3. The code is very readable. It says that there are PEP-8 violations for your 5 functions you defined at the beginnning "E302 expected 2 blank lines, found 1". There are also some variable names and comments under your functions that are giving errors related to indentation.
+# 3. The code is readable. However it says that there are PEP-8 violations for your 5 functions you defined at the beginnning "E302 expected 2 blank lines, found 1". There are also some variable names and comments under your functions that are giving errors related to indentation.
 #
 #
 # 4. All of the current comments describe the code well, but I would add more comments, specifically where all of your functions are. It would help to decribe what each functions are doing. There are also some spelling and spacing mistakes in the last few comments.
@@ -141,7 +141,7 @@ else:
 # 7. The script does follow a functional programming paradigm. There are seperate functions and steps for everything that needs to be calculated.
 #
 #
-# 8. There are parts of the table that are unclear, specifically the the first and last velocities say N/A on the output. I believe you are still fixing those so it most likely just caused by that.
+# 8. There are parts of the table that are unclear, specifically the the first and last velocities say N/A on the output. I believe you are still fixing those so it most likely just caused by that. The graph looks good.
 #
 #
 ########################################################################
